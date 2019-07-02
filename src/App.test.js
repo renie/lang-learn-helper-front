724642f3cff2e3f618142f1e7166b05ab2751bf2
Renie Siqueira <renie.webdev@gmail.com>
2019-07-02 17:00:11 -0300

    Changes: 	- Snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import renderer from 'react-test-renderer'
+
+const useSelector = fn => {
+    fn({wordState: { words: [] }})
+}
+const useDispatch = () => ({})
 
 it('renders without crashing', () => {
-    const useSelector = fn => fn({wordState: { words: [] }})
-    const useDispatch = () => ({})
-
     const div = document.createElement('div')
-    ReactDOM.render(<App useSelectorFn={useSelector} useDispatchFn={useDispatch}/>, div)
+    ReactDOM.render(
+        <App 
+            useSelectorFn={useSelector}
+            useDispatchFn={useDispatch}/>, div)
+
     ReactDOM.unmountComponentAtNode(div)
+});
+
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<App 
+            useSelectorFn={useSelector}
+            useDispatchFn={useDispatch}/>)
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
 });
