586230d28e5e91040de884c2abe27fa472953810
Renie Siqueira <renie.webdev@gmail.com>
2019-07-02 16:51:47 -0300

    Changes: 	- Tests fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const useSelector = fn => fn({wordState: { words: [] }})
+    const useDispatch = () => ({})
+
+    const div = document.createElement('div')
+    ReactDOM.render(<App useSelectorFn={useSelector} useDispatchFn={useDispatch}/>, div)
+    ReactDOM.unmountComponentAtNode(div)
 });
