02f54c8fa613c361a1fe2b3e71aa3c5a951c41e2
Renie Siqueira <renie.webdev@gmail.com>
2019-07-07 21:24:50 -0300

    Changes: 	- Essay tests part 2

--- src/essay/EssayEditorLogic.test.js
+++ src/essay/EssayEditorLogic.test.js
@@ -1,21 +1,28 @@
 import EssayEditorLogic from './EssayEditorLogic'
 
-const logic = EssayEditorLogic([{
-                    "name":"used",
-                    "meanings":[{
-                        "id": 1,
-                        "wordClass": "verb",
-                        "example": "He used my pen.",
-                        "description": "Verb use in past tense."
-                    },
-                    {
-                        "id":2,
-                        "wordClass":"adjective",
-                        "example":"My pen is used.",
-                        "description":"Means the object is not new anymore."
-                    }],
-                    "_id":"BcNdVZy038MWxqrg"
-                }])
+const words = [{
+        "name":"used",
+        "meanings":[{
+            "id": 1,
+            "wordClass": "verb",
+            "example": "He used my pen.",
+            "description": "Verb use in past tense."
+        },
+        {
+            "id":2,
+            "wordClass":"adjective",
+            "example":"My pen is used.",
+            "description":"Means the object is not new anymore."
+        }],
+        "_id":"BcNdVZy038MWxqrg"
+    }]
+
+const baloon = {id: 'baloon'}
+const WordMarkBaloon = jest.fn(() => baloon)
+const WordMark = jest.fn()
+
+const logic = EssayEditorLogic({words, WordMark, WordMarkBaloon})
+
 
 it('sendCursorToEnd', () => {
     const editor = { name:'editor' }
@@ -41,4 +48,23 @@ it('sendCursorToEnd', () => {
     expect(range.collapse).toHaveBeenCalledWith(false)
     expect(selection.removeAllRanges).toHaveBeenCalled()
     expect(selection.addRange).toHaveBeenCalledWith(range)
+})
+
+it('getWordObj', () => {
+    expect(logic.getWordObj('used')).toBe(words[0])
+    expect(logic.getWordObj('nope')).toBe(undefined)
+})
+
+it('tagWord', () => {
+    logic.tagWord('used', words[0])
+    expect(WordMarkBaloon).toHaveBeenCalledWith(words[0])
+    expect(WordMark).toHaveBeenCalledWith('used', words[0], baloon)
+})
+
+it('breakTextInWords', () => {
+    expect(logic.breakTextInWords('not used')).toStrictEqual(['not', 'used'])
+})
+
+it('putTextTogether', () => {
+    expect(logic.putTextTogether(['not', 'used'])).toStrictEqual('not used')
 })
